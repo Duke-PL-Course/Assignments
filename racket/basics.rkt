34f2543619aeeab185e73d5f0a5b50748a304d1d
Jim Posen <jim.posen@gmail.com>
2013-09-03 22:44:35 -0400

    Showed solutions to in class exercises.

--- racket/basics.rkt
+++ racket/basics.rkt
@@ -1,19 +1,43 @@
 #lang racket
 
 ;;; Return the sum of all numbers up to and including n for 0 <= n.
-(define (sum-to-n n) null)
+(define (sum-to-n n)
+  (cond
+   ((zero? n) 0)
+   (else
+    (+ n (sum-to-n (- n 1))))))
 
 ;;; Calculate n choose r, for 0 <= n, 0 <= r <= n
-(define (combination n r) null)
+(define (combination n r)
+  (cond
+   ((zero? r) 1)
+   ((= n r) 1)
+   (else
+    (+ (combination (- n 1) (- r 1))
+       (combination (- n 1) r)))))
 
 ;;; Return the sum of all numbers in a list
-(define (sum lst) null)
+(define (sum lst)
+  (cond
+   ((null? lst) 0)
+   (else
+    (+ (car lst) (sum (cdr lst))))))
 
 ;;; Return whether element a is contained in list lst
-(define (member? a lst) null)
+(define (member? a lst)
+  (cond
+   ((null? lst) #f)
+   ((equal? a (car lst)) #t)
+   (else
+    (member? a (cdr lst)))))
 
 ;;; Return a list with the *first* instance of element a removed if present
-(define (rember lst a) null)
+(define (rember lst a)
+  (cond
+   ((null? lst) '())
+   ((equal? a (car lst)) (cdr lst))
+   (else
+    (cons (car lst) (rember (cdr lst) a)))))
 
 ;;; Return whether element a is contained in any level of S-expression lst
 (define (member?* a lst) null)
