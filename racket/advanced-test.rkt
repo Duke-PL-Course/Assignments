43045ee7586d2a0b1d5de83cfae889bfb2ca7a03
Jim Posen <jim.posen@gmail.com>
2013-09-17 15:02:48 -0400

    New problems for 4th lecture.

--- racket/advanced-test.rkt
+++ racket/advanced-test.rkt
@@ -9,3 +9,40 @@
  (check-equal? (fibonacci 2) 1)
  (check-equal? (fibonacci 10) 55)
  (check-equal? (fibonacci 100) 354224848179261915075))
+
+(test-case
+ "qsort"
+ (check-equal? (qsort '()) '())
+ (check-equal? (qsort '(1)) '(1))
+ (check-equal? (qsort '(1 2 3)) '(1 2 3))
+ (check-equal? (qsort '(3 2 1)) '(1 2 3))
+ (check-equal? (qsort '(3 4 1 5 2)) '(1 2 3 4 5))
+ (check-equal? (qsort '(3 4 1 5 2 3 3)) '(1 2 3 3 3 4 5)))
+
+(test-case
+ "reverse"
+ (check-equal? (reverse '()) '())
+ (check-equal? (reverse '(a)) '(a))
+ (check-equal? (reverse '(a b c d)) '(d c b a)))
+
+(test-case
+ "prime?"
+ (check-false (prime? 1))
+ (check-true (prime? 2))
+ (check-true (prime? 3))
+ (check-false (prime? 4))
+ (check-false (prime? 91))
+ (check-true (prime? 109)))
+
+(test-case
+ "product-of-digits"
+ (check-equal? (product-of-digits 0) 0)
+ (check-equal? (product-of-digits 6) 6)
+ (check-equal? (product-of-digits 123) 6)
+ (check-equal? (product-of-digits 99) 81))
+
+(test-case
+ "maximum"
+ (check-equal? (maximum '(1 2 3)) 3)
+ (check-equal? (maximum '(-1 3 -2)) 3)
+ (check-equal? (maximum '(-1)) -1))
--- racket/advanced.rkt
+++ racket/advanced.rkt
@@ -3,5 +3,28 @@
 ;; The nth element of the Fibonacci sequence. 0, 1, 1, 2, 3, 5...
 (define (fibonacci n) nil)
 
+;; Quick sort
+(define (qsort lst) nil)
+
+;; Reverse the list
+(define (reverse lst) nil)
+
+;; Check if n is prime
+(define (prime? n) nil)
+
+;; Find maximum element of list using > operator
+(define (maximum lst) nil)
+
+;; Find the product of the digits of a non-negative integer n
+(define (product-of-digits n) nil)
+
+;; Find the greatest number which is the product of the digits of a prime less than n
+(define (greatest-prime-digit-product n) nil)
+
 (provide
- fibonacci)
+ fibonacci
+ qsort
+ prime?
+ maximum
+ product-of-digits
+ greatest-prime-digit-product)
