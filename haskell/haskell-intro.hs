778a4fa6f29a0a16c030e6224c82535bed5e8d9c
Jim Posen <jim.posen@gmail.com>
2013-10-01 15:02:55 -0400

    Exercises for week 6.

---
+++ haskell/haskell-intro.hs
@@ -0,0 +1,17 @@
+-- Problem 1: Write myZip which zips two lists
+
+
+-- Problem 2: Write qsort implementing quicksort
+
+
+-- Problem 3: Write function fib n, that computes the nth number in the fibonacci sequence in 0(n) time
+-- Problem 3 Bonus: Construct an infinite sequence of fibonacci numbers, fibs
+
+
+-- Problem 4: http://projecteuler.net/problem=6
+
+
+-- Problem 5: http://projecteuler.net/problem=9
+
+
+-- Problem 6: http://projecteuler.net/problem=5
