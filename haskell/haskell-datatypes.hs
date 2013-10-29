5fa6533304a5c65c7b7b381d7935ed40dde45813
Jim Posen <jim.posen@gmail.com>
2013-10-29 16:28:10 -0400

    Exercises for Week 9.

---
+++ haskell/haskell-datatypes.hs
@@ -0,0 +1,15 @@
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+insert :: (Ord a) => a -> Tree a -> Tree a
+
+contains :: (Ord a) => a -> Tree a -> Bool
+
+toList :: Tree a -> [a]
+
+remove :: Tree a -> a -> Tree a
+
+bstSort :: (Ord a) => [a] -> [a]
+
+printInOrder :: (Show a) => Tree a -> IO ()
+
+sortStream :: IO [Int]
