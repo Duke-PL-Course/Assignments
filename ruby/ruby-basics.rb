644e73c5d638aa32d0e0607a4189600ad0d4691a
Jim Posen <jim.posen@gmail.com>
2013-11-12 16:26:41 -0500

    First Ruby assignment.

---
+++ ruby/ruby-basics.rb
@@ -0,0 +1,32 @@
+##
+# Implement any sorting algorithm.
+# Bonus: keep it concise
+#
+def sort(array)
+
+end
+
+##
+# Longest Collatz sequence
+#
+def euler14(n)
+
+end
+
+##
+# Return a random permutation of the elements.
+#
+def shuffle(array)
+
+end
+
+##
+# Perform a Rot13 transformation on a string.
+#
+def rot13(string)
+
+end
+
+def query_classes(data, criteria)
+
+end
