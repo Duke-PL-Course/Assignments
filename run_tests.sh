3cae732391d52ddea48fdae61b8edeace9078099
Jim Posen <jim.posen@gmail.com>
2013-12-03 17:38:00 -0500

    Fixed run_tests.sh.

--- run_tests.sh
+++ run_tests.sh
@@ -6,11 +6,11 @@ racket advanced-test.rkt
 cd ..
 
 cd haskell
-ghc haskell-intro-solutions.hs haskell-intro-test.hs
+ghc haskell-intro.hs haskell-intro-test.hs
 ./haskell-intro-test
-ghc haskell-typing-solutions.hs haskell-typing-test.hs
+ghc haskell-typing.hs haskell-typing-test.hs
 ./haskell-typing-test
-ghc haskell-datatypes-solutions.hs haskell-datatypes-test.hs
+ghc haskell-datatypes.hs haskell-datatypes-test.hs
 ./haskell-datatypes-test
 rm *.o *.hi haskell-intro-test haskell-typing-test haskell-datatypes-test
 cd ..
